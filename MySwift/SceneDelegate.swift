bac142981ef92aeab7e9009d785a99e3e2e73854
huangyouli <1402952307@qq.com>
2023-11-16 11:06:58 +0800

    创建Swift项目, pod 导入 SnapKit 布局

---
+++ MySwift/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  MySwift
+//
+//  Created by zhou on 2023/11/16.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+}
+
---
+++ MySwift/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MySwift
+//
+//  Created by zhou on 2023/11/16.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let view = UIView()
+        view.backgroundColor = .orange
+        self.view.addSubview(view)
+        view.snp.makeConstraints { make in
+            make.width.equalTo(120)
+            make.height.equalTo(120)
+            make.center.equalTo(self.view)
+        }
+        
+    }
+
+
+}
+
